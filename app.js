4f9a09b8942349bad60c5daba9b8ddd09e4c3db2
aominedaik <79418732+aominedaik@users.noreply.github.com>
2022-04-04 15:39:09 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,202 @@
+const sections = document.querySelectorAll('.section');
+const sectBtns = document.querySelectorAll('.controlls');
+const sectBtn = document.querySelectorAll('.control');
+const allSections = document.querySelector('.main-content');
+//section1
+var SN1_1 = "Hi, I'm <span>Sirine NSIR.</span>";
+var SN1_2 = "Computer Vision and Embedded AI engineer";
+var SN1_3 = "Hi, I'm Sirine NSIR Computer Vision and Embedded AI engineer I'm interested in Artificial Intelligence and Web develpement .<br /> I consider myself as punctual, perfectionist, and very enthusiastic person.<br /> Last but not least I am as effective in a group as by my own and my interpersonal skills are my superpower .";
+var SN1_4 = "img/SN.jpeg"
+
+var SO1_1= "Hi, I'm <span>OMRANI Sirine.</span>"
+var SO1_2= "An Electronic Engineering Student."
+var SO1_3= "I'm specialized in Microelectronics and Embedded Systems. I really like the field of artificial intelligence and everything related to computer vision. I also want to learn more about web development to be able to manage the different given projects."
+var SO1_4 = "img/sirine.jpg"
+
+var O1_1=" Hi, I'm <span>Oumaima ZRIBI.</span>"
+var O1_2="Data scientist."
+var O1_3=" I'm a data scientist. I was hocked by the artificial intelligence, its different field and its possible realisations that's why I chose it as a career."
+var O1_4="img/me.png"
+
+document.getElementById("section1_1").innerHTML= SO1_1;
+document.getElementById("section1_2").innerHTML= SO1_2;
+document.getElementById("section1_3").innerHTML= SO1_3;
+document.querySelector('img').src= SO1_4;
+
+//section2
+var O2_1=" I'm a data science engineering student. At the moment, I'm occupied by fulfilling my internship in DB consulting group. For me it's more than just an internship. It's actually an occasion to learn and master new technologies and the most important, it's an opportunity to expand my abilities."
+var SO2_1= "I'm a microelectrnics engineering student. During this period, I'm occupied by my end-of-study internship in DB CONSULTING group.<br/>I consider this as an opportunity to master new technologies and  practise my knowledge in the IA and Computer Vision field ."
+var SN2_1="This is Sirine Nsir, from Tunisia. I am 25-years-old. I am an Engineering Student at the High Institute of Computer Science and Mathematics of Monastir (ISMM), in Tunisia. I obtained my baccalaureate diploma in natural science in 2016 and a B.A. in Electronicand informatics in 2019. I have recently taken a decision to pursue my studies in the area of Electronic- Microelectronics engineering. My greatest strength is my ability to work well in a team environment. I really love working as part of a wider multi-cultural team. I am someone who sees into the big picture : I dream of initiating my own project/product from the scratch until it becomes something common not only in my country but in the entire world. I am an innovative and creative person. I have many projects ideas to do for this year and I am doing my best and trying to snatch every single opportunity to take part in renowned international challenges… I am interested in Embedded Systems and Web developement and I would like to become a certified expert in this area."
+var SO2_2 = "13+";  
+var SO2_3="Projects <br /> Completed";
+var SO2_4= "0";
+var SO2_5="Years of <br /> experience";
+var SO2_6= "In the near future";
+var SO2_7="Happy <br /> Clients";
+var SN2_2= " Smart Pharmacy Box"
+var SN2_3="KeyWords <br /> #Pic 18F4550 <br /> #GSM 800 L <br /> #PIC C Compiler <br /> #LCD <br /> #Motion sensor <br /> #PCB Design <br />#ISIS ";
+var SN2_4=" Raspberry-Pi face Mask Detection";
+var SN2_5="KeyWords <br /> #Tensorflow #Keras,<br /> #OpenCV <br /> #Python <br /> #RaspberryPi <br /> #Machine Learning <br /> #Real Time ";
+var SN2_6="Arceau parking";
+var SN2_7="KeyWords <br /> #ISIS <br /> #Android_app <br /> #Arduino <br /> #sensors <br /> #PCB Design <br /> #ISIS ";
+var O2_2="20+"
+var O2_3="Projects <br /> Completed"
+var O2_4="0"
+var O2_5="Years of <br /> experience"
+var O2_6="In the near future"
+var O2_7="Happy <br /> Clients"
+var SN2_8="C/C++"
+var SN2_9="85%"
+var SN2_10="Python "
+var SN2_11="90%"
+var SN2_12="ISIS "
+var SN2_13="90% "
+var SN2_14="LABVIEW"
+var SN2_15="50%"
+var SN2_16="My skills "
+var SO2_8 ="Raspberry"
+var SO2_9 ="80%"
+var SO2_10 ="Solidworks"
+var SO2_11 ="65%"
+var SO2_12 ="Python65%"
+var SO2_13 ="75%"
+var SO2_14 ="Artificial intelligence"
+var SO2_15 ="90%"
+var SO2_16 ="My Skills"
+var O2_8= "Machine Learning"
+var O2_9="80%"
+var O2_10= "Deep Learning"
+var O2_11="80%"
+var O2_12="Analatycal statistics"
+var O2_13="70%"
+var O2_14="Business Intelligence"
+var O2_15="80%"
+var O2_16= "My Skills"
+var O2_17="2019 - present"
+var O2_18="Data science engineering student<span> - ENET'COM</span>"
+var O2_19="Embedded systems and telecommunication institute of Sfax"
+var O2_20="2017 - 2019"
+var O2_21="Preparatory student<span> - Mathematics-Phusics</span>"
+var O2_22=" Preparatory institute of Monastir"
+var O2_23="2017"
+var O2_24="bachelor degree<span> Mathematics"
+var O2_25="high school Ibn khaldoun"
+
+var SO2_17= "2019-PRESENT"
+var SO2_18= "Microelectronics and Embedded System engineering student<span> - ISIMM</span>"
+var SO2_19= "Higher Institute of Computer Science and Mathematics of Monastir"
+var SO2_20= "2016-2019"
+var SO2_21= "Basic license in Science and technology of information and telecommunications<span> (STIC) - ISIMM</span>"
+var SO2_22= "Higher Institute of Computer Science and Mathematics of Monastir"
+var SO2_23= "2016"
+var SO2_24= "Bachelor degree<span> Mathematics</span></h5>" 
+var SO2_25= "High school Jemmel 1"
+var SN2_17="TUNISIA : 2019-2022"
+var SN2_18="Higher institute of Computer science and Mathematics of Monastir Monastir | Tunisia"
+var SN2_19=" Electronic & MICROELECTRONIC ENGINEERING "
+var SN2_20="TUNISIA : 2016-2019"
+var SN2_21=" Higher institute of Computer science and Mathematics of Monastir Monastir | Tunisia"
+var SN2_22="Fundamental licence in Information , Communication Sciences and Technologies Electronics "
+var SN2_23="TUNISIA : 2012-2016"
+var SN2_24="Abdel Aziz El Behi "
+var SN2_25="Bachelor's degree in Experimental Sciences "
+
+document.getElementById("section2_1").innerHTML= SN2_1;
+document.getElementById("section2_2").innerHTML= SN2_2;
+document.getElementById("section2_3").innerHTML= SN2_3;
+document.getElementById("section2_4").innerHTML= SN2_4;
+document.getElementById("section2_5").innerHTML= SN2_5;
+document.getElementById("section2_6").innerHTML= SN2_6;
+document.getElementById("section2_7").innerHTML= SN2_7;
+document.getElementById("section2_8").innerHTML= SN2_8;
+document.getElementById("section2_9").innerHTML= SN2_9;
+document.getElementById("section2_10").innerHTML= SN2_10;
+document.getElementById("section2_11").innerHTML= SN2_11;
+document.getElementById("section2_12").innerHTML= SN2_12;
+document.getElementById("section2_13").innerHTML= SN2_13;
+document.getElementById("section2_14").innerHTML= SN2_14;
+document.getElementById("section2_15").innerHTML= SN2_15;
+document.getElementById("section2_16").innerHTML= SN2_16;
+document.getElementById("section2_17").innerHTML= SN2_17;
+document.getElementById("section2_18").innerHTML= SN2_18;
+document.getElementById("section2_19").innerHTML= SN2_19;
+document.getElementById("section2_20").innerHTML= SN2_20;
+document.getElementById("section2_21").innerHTML= SN2_21;
+document.getElementById("section2_22").innerHTML= SN2_22;
+document.getElementById("section2_23").innerHTML= SN2_23;
+document.getElementById("section2_24").innerHTML= SN2_24;
+document.getElementById("section2_25").innerHTML= SN2_25;
+
+//section3
+var O3_1="Here are some fields that I'm interested in"
+var O3_2="img/cloud.jpg"
+var O3_3="Cloud"
+var O3_4="img/data-mining.jpg"
+var O3_5=" Data mining"
+var O3_6="img/ML-DL.jpg"
+var O3_7="ML & DL"
+document.getElementById("section3_1").innerHTML= O3_1;
+document.getElementById("section3_2").src = O3_2;
+document.getElementById("section3_3").innerHTML= O3_3;
+document.getElementById("section3_4").src = O3_4;
+document.getElementById("section3_5").innerHTML= O3_5;
+document.getElementById("section3_6").src = O3_6;
+document.getElementById("section3_7").innerHTML= O3_7;
+
+
+//section4
+
+//section5
+SO5_2 ="MONASTIR, Tunisia"
+SO5_3="sirine.o@db-consulting-group.com"
+SO5_4="ISIMM University"
+SO5_5="92775913"
+SO5_6="Arabic, English, French"
+document.getElementById("section5_1").innerHTML= "";
+document.getElementById("section5_2").innerHTML= SO5_2;
+document.getElementById("section5_3").innerHTML= SO5_3;
+document.getElementById("section5_4").innerHTML= SO5_4;
+document.getElementById("section5_5").innerHTML= SO5_5;
+document.getElementById("section5_6").innerHTML= SO5_6;
+
+
+
+function PageTransitions(){
+    //Button click active class
+    for(let i = 0; i < sectBtn.length; i++){
+        sectBtn[i].addEventListener('click', function(){
+            let currentBtn = document.querySelectorAll('.active-btn');
+            currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
+            this.className += ' active-btn';
+        })
+    }
+
+    //Sections Active 
+    allSections.addEventListener('click', (e) =>{
+        const id = e.target.dataset.id;
+        if(id){
+            //resmove selected from the other btns
+            sectBtns.forEach((btn) =>{
+                btn.classList.remove('active')
+            })
+            e.target.classList.add('active')
+
+            //hide other sections
+            sections.forEach((section)=>{
+                section.classList.remove('active')
+            })
+
+            const element = document.getElementById(id);
+            element.classList.add('active');
+        }
+    })
+
+    //Toggle theme
+    const themeBtn = document.querySelector('.theme-btn');
+    themeBtn.addEventListener('click',() =>{
+        let element = document.body;
+        element.classList.toggle('light-mode')
+    })
+}
+
+PageTransitions();
